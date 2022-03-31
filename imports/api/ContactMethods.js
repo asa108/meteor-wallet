ba7c276f57aadb397228f21a3e16f7603009bd13
ayara@safeandreliablecare.com <ayara@safeandreliablecare.com>
2022-03-30 22:59:17 -0400

    implemented meteor methods and optimistic ui

--- imports/api/ContactMethods.js
+++ imports/api/ContactMethods.js
@@ -1,4 +1,6 @@
 import { ContactCollection } from "./ContactCollection";
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
 
 // ContactCollection.SimpleSchema({
 //   name: {
@@ -14,12 +16,21 @@ import { ContactCollection } from "./ContactCollection";
 
 Meteor.methods({
   "contact.insert"({ name, email, imageUrl }) {
-    // check(name, String);
-    // check(email, String);
-    // check(imageUrl, String);
+    check(name, String);
+    check(email, String);
+    check(imageUrl, String);
     if (!name) {
       throw new Meteor.Error("Name is required");
     }
-    return ContactCollection.insert({ name, email, imageUrl });
+    return ContactCollection.insert({
+      name,
+      email,
+      imageUrl,
+      createdAt: new Date(),
+    });
+  },
+  "contact.remove"({ contactId }) {
+    check(contactId, String);
+    return ContactCollection.remove(contactId);
   },
 });
